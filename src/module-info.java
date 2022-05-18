61a519a8cd74b01fcff91be1c42ab1eb30127500
pbsun <pbsun@LAPTOP-0NBRK4L8>
2022-05-18 00:38:22 -0500

    Archivos necesarios para el proyecto

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module tarea2 {
+}
---
+++ src/tarea2_repo_ps/Main.java
@@ -0,0 +1,10 @@
+package tarea2_repo_ps;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOLA MUNDO");
+	}
+
+}
